e3181545cff67633c197cf554eaf2268c1321ca2
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 14:43:21 -0300

    Add project files.

---
+++ ExercicioArrayVogalConsoante/Program.cs
@@ -0,0 +1,25 @@
+﻿char[] palavra = new char[10];
+
+for (int i = 0; i < 10; i++)
+{
+    palavra[i] = char.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Vogais");
+for (int i = 0; i < 10; i++)
+{
+    if (palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+    {
+        Console.Write(palavra[i]);
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("Consoantes");
+for (int i = 0; i < 10; i++)
+{
+        if (palavra[i] != 'a' && palavra[i] != 'e' && palavra[i] != 'i' && palavra[i] != 'o' && palavra[i] != 'u')
+        {
+            Console.Write(palavra[i]);
+        }
+}
